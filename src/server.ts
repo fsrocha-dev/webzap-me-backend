da8b29206278efb80640fe274cea1b2f4ade9103
Frank Rocha <contato.frankrocha@gmail.com>
2022-09-27 14:14:14 -0300

    feat: inital server

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+import app from './app';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`webzap.me rodando na porta: ${PORT}`));
