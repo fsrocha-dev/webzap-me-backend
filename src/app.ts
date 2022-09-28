a364ead97f26ac6f8dc43b7de1954d6304cf0bd6
Frank Rocha <contato.frankrocha@gmail.com>
2022-09-27 22:10:57 -0300

    fix: add router and error middleware to server

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import cors from 'cors';
 import express, { json } from 'express';
 import 'express-async-errors';
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
+import router from './routes';
 
 const app = express();
 app.use(cors());
 app.use(json());
+app.use(router);
+app.use(errorHandlerMiddleware);
 
 export default app;
