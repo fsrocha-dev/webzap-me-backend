608451f3d861901b1dcdc62ea1b31da728fd2e28
Frank Rocha <contato.frankrocha@gmail.com>
2022-09-28 08:29:57 -0300

    feat: add create user function

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,7 +1,28 @@
+import { hash } from 'bcrypt';
+import * as userRepository from '../repositories/userRepository';
 import { ICreateUser } from '../repositories/userRepository';
+import { conflictError, notFoundError } from '../utils/errorUtils';
 
-async function getUserById(id: string) {}
+async function getUserById(id: string) {
+  const user = await userRepository.getUserById(id);
+  if (!user) return notFoundError('Usuário não encontrado!');
 
-async function makeUser(user: ICreateUser) {}
+  return user;
+}
+
+async function makeUser(user: ICreateUser) {
+  const userExist = await userRepository.getUserByMail(user.email);
+
+  if (userExist) return conflictError('Favor, tente usar outro e-mail!');
+
+  const hashPassword = await hash(user.password, 10);
+
+  const { password, ...createdUser } = await userRepository.createUser({
+    ...user,
+    password: hashPassword
+  });
+
+  return createdUser;
+}
 
 export { getUserById, makeUser };
