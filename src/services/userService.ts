b50dbc89a5276a928e7813f25c3a668fd17bb023
Frank Rocha <contato.frankrocha@gmail.com>
2022-09-27 22:09:18 -0300

    feat: add user service base

---
+++ src/services/userService.ts
@@ -0,0 +1,7 @@
+import { ICreateUser } from '../repositories/userRepository';
+
+async function getUserById(id: string) {}
+
+async function makeUser(user: ICreateUser) {}
+
+export { getUserById, makeUser };
