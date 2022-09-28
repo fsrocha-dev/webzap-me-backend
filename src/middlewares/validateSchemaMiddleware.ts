027cd7aa7f1966b7816cb6fd885afe346f20c4ad
Frank Rocha <contato.frankrocha@gmail.com>
2022-09-28 08:28:15 -0300

    fix: object error response joi

--- src/middlewares/validateSchemaMiddleware.ts
+++ src/middlewares/validateSchemaMiddleware.ts
@@ -5,7 +5,9 @@ export function validateSchemaMiddleware(schema: ObjectSchema) {
   return (req: Request, res: Response, next: NextFunction) => {
     const validation = schema.validate(req.body);
     if (validation.error) {
-      return res.status(422).send(validation.error);
+      // Entrei dentro do objeto o máximo possível, para retornar somente a mensagem de erro do JOI.
+      // Preferi optar por mostrar uma mensagem de cada vez dos erros de validação que acontecem via JOI
+      return res.status(422).send(validation.error.details[0].context.label);
     }
 
     next();
