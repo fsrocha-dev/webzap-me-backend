85fc0426a840aa1766d2e9975f4217ce1be110f5
Frank Rocha <contato.frankrocha@gmail.com>
2022-09-30 10:43:53 -0300

    feat: add lead controller

---
+++ src/controllers/leadController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import * as service from '../services/leadService';
+
+export async function store(req: Request, res: Response) {
+  const { ref } = req.params;
+  const leadInfo = req.body;
+
+  await service.createLead(leadInfo, ref);
+
+  res.status(200);
+}
