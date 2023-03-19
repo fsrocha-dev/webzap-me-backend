2b5b018ec22151adc3df067f253be62ad1b01d5b
Frank Rocha <contato.frankrocha@gmail.com>
2023-03-19 18:39:57 -0300

    fix: local port server

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import app from './app';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`webzap.me rodando na porta: ${PORT}`));
