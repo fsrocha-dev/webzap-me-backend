ecbc17ddae8a386ddc993673db9d15227ef5249e
Frank Rocha <contato.frankrocha@gmail.com>
2023-03-19 18:39:45 -0300

    fix: public router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,12 @@ import authRouter from './authRouter';
 import campaignRouter from './campaignRouter';
 import leadRouter from './leadRouter';
 import linkRouter from './linkRouter';
+import publicRouter from './publicRouter';
 import userRouter from './userRouter';
 
 const router = Router();
 
+router.use(publicRouter);
 router.use(leadRouter);
 router.use(userRouter);
 router.use(authRouter);
