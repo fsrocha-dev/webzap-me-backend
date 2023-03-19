47719fc363e8e4f6f2db633149bfc35f2a7aea2c
Frank Rocha <contato.frankrocha@gmail.com>
2023-03-19 18:40:46 -0300

    fix: add whatsapp link to schema validation

--- src/schemas/linkSchema.ts
+++ src/schemas/linkSchema.ts
@@ -7,5 +7,6 @@ export const linkCreateDataSchema = joi.object({
     .label('Numero de telefone deve ter 11 digitos')
     .required(),
   custom_message: joi.string().required().label('Informe a mensagem do link'),
-  is_active: joi.bool().required().label('Status deve ser true ou false')
+  whatsapp_link: joi.string().required().label('Url do whatapp é obrigatório'),
+  is_active: joi.bool().label('Status deve ser true ou false')
 });
