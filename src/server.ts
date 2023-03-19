b4a63749a4a38bb63cdb3d5688bbb89910222f2e
Frank Rocha <contato.frankrocha@gmail.com>
2023-03-19 19:03:20 -0300

    chore: deploy render configuration

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import app from './app';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`webzap.me rodando na porta: ${PORT}`));
